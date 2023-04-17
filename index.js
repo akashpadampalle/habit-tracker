8f18626a930b3ace7a678add5fc8496f448cedf0
Akash Padampalle <akashpadampalle780@gmail.com>
2023-04-17 18:14:25 +0530

    add database connection

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./configs/dbconnection.js');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+
+app.listen(PORT, (err) => {
+    if(err){ console.log(err); return; }
+    console.log(`server is up and running :: PORT ${PORT}`)
+});
